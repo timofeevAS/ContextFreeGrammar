7f82b38eb0c0a836e767495d87c38b9b61444d7a
timofeevAS <a.timoff3@gmail.com>
2024-04-18 21:31:53 +0300

    On FreeContextGrammar: modify NonTerminalWord class

--- src/main/kotlin/NonTerminalWord.kt
+++ src/main/kotlin/NonTerminalWord.kt
@@ -1,2 +1,17 @@
-class NonTerminalWord(value: String, expressionList: MutableList<Sequence>): Word(value) {
+class NonTerminalWord(value: String, private val expressionList: MutableList<Sequence>): Word(value) {
+    fun getExpressionList():MutableList<Sequence>{
+        return expressionList
+    }
+
+    fun addSequence(sequence: Sequence){
+        expressionList.add(sequence)
+    }
+
+    override fun toString():String {
+        return "<$value>"
+    }
+
+    fun listToString():String {
+        return ""
+    }
 }
