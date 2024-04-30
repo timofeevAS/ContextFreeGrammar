1a17bb26aa8bd0fc6d5ba43282d0947cd786e502
timofeevAS <a.timoff3@gmail.com>
2024-04-30 19:02:01 +0300

    ll analyze

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,12 @@ fun main(args: Array<String>) {
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    Grammar("src/main/resources/rules")
+    val grammar = Grammar("src/main/resources/rules")
+    val ans = grammar.recognizeSequence(listOf(TerminalWord("Cool"),
+                                     TerminalWord("Man"),
+                                     TerminalWord("has"),
+                                     TerminalWord("Ran"),
+                                     TerminalWord(".")))
+
+    println(ans)
 }
